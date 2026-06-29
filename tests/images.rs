a187a20d2b2a2253c3b074e46d7798690fa8957d
Colin Marc <hi@colinmarc.com>
2026-06-29 23:00:31 +0200

    test: add a test case for `EmptyResponse` and `Request` as extractor

--- tests/images.rs
+++ tests/images.rs
@@ -1,3 +1,4 @@
+use gropius::{EmptyResponse, Request, Response};
 use schemars::JsonSchema;
 use serde::Serialize;
 
@@ -15,19 +16,26 @@ impl gropius::ApiError for ApiError {
 #[gropius::api]
 trait ImageApi {
     #[endpoint(GET, "/image", content_type = "image/png")]
-    async fn get_image(&self) -> Result<gropius::Response, ApiError>;
+    async fn get_image(&self) -> Result<Response, ApiError>;
+
+    #[endpoint(POST, "/image")]
+    async fn upload_image(&self, request: Request) -> Result<EmptyResponse, ApiError>;
 }
 
 #[derive(Clone)]
 struct Server;
 
 impl ImageApi for Server {
-    async fn get_image(&self) -> Result<gropius::Response, ApiError> {
+    async fn get_image(&self) -> Result<Response, ApiError> {
         Ok(http::Response::builder()
             .status(200)
             .header("content-type", "image/png")
             .body(bytes::Bytes::from_static(b"\x89PNG fake image"))
             .unwrap())
+    }
+
+    async fn upload_image(&self, _request: Request) -> Result<EmptyResponse, ApiError> {
+        Ok(EmptyResponse)
     }
 }
 
@@ -36,11 +44,20 @@ async fn api_impl() -> anyhow::Result<()> {
     let srv = Server;
     let router = gropius::Router::new(srv.endpoints())?;
 
-    let req = http::Request::get("/image").body(bytes::Bytes::new())?;
-    let resp = router.dispatch(req).await;
-    assert_eq!(resp.status(), 200);
-    assert_eq!(resp.headers().get("content-type").unwrap(), "image/png");
-    assert_eq!(resp.body().as_ref(), b"\x89PNG fake image");
+    {
+        let req = http::Request::get("/image").body(bytes::Bytes::new())?;
+        let resp = router.dispatch(req).await;
+        assert_eq!(resp.status(), 200);
+        assert_eq!(resp.headers().get("content-type").unwrap(), "image/png");
+        assert_eq!(resp.body().as_ref(), b"\x89PNG fake image");
+    }
+
+    {
+        let req = http::Request::post("/image").body(bytes::Bytes::from_static(b"\x89PNG"))?;
+        let resp = router.dispatch(req).await;
+        assert_eq!(resp.status(), 200);
+        assert!(resp.body().is_empty());
+    }
 
     Ok(())
 }
